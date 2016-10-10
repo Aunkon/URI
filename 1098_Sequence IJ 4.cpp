e3b01b66998a3f0217fb1f6a905a4721cdb3c6af
Aunkon <mdwalidbinkhalidaunkon@gmail.com>
2016-10-10 17:34:17 +0600

    Revert "URI"
    This reverts commit 8675d44ffc64f6e80e1055f1c9a89e63c5ecf8fa.

--- 1098_Sequence IJ 4.cpp
+++ 1098_Sequence IJ 4.cpp
@@ -3,16 +3,3 @@
 /****       ID:     121-15-1669                          ****/
 /****       Email:  mdwalidbinkhalidaunkon@gmail.com     ****/
 /****       Mobile No:  +88-01916-492926                 ****/
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    double i,j=1;
-    for(i=0;i<=2;i+=.2)
-    {
-        cout << "I=" << i << " " << "J=" << j+i << "\n";
-        cout << "I=" << i << " " << "J=" << j+i+1 << "\n";
-        cout << "I=" << i << " " << "J=" << j+i+2 << "\n";
-    }
-    return 0;
-}
